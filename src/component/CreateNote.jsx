c0c1d62859dfeb64964c9ffa9aa23a874234a415
webpawan <pawansaini66477@gmail.com>
2022-10-21 20:57:04 +0530

    tag functionality

--- src/component/CreateNote.jsx
+++ src/component/CreateNote.jsx
@@ -11,47 +11,58 @@ const getlocalstorage = () => {
 };
 
 const CreateNote = () => {
-  const [input, setInput] = useState({ id: "", title: "", desc: ""});
+  const [input, setInput] = useState({ id: "", title: "", desc: "" });
   const [list, setList] = useState(getlocalstorage());
   const [isediting, setisediting] = useState(false);
   const [myid, setid] = useState(null);
-const [color, setcolor] = useState('white');
-
+  const [color, setcolor] = useState("");
+  const [tags, setTags] = useState({tag:""});
+  
   const inputhandler = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
   };
 
-  const pickcolor = (e) =>{
-setcolor(e.target.id);
+  const pickcolor = (e) => {
+    setcolor(e.target.id);
+  };
 
-  }
-
-
-  
+  const pickTag = (e) => {
+    let mytag = e.target.className;
+    
+   
+    setTags({tag:mytag});
+  };
 
   const setdata = (e) => {
-    
     e.preventDefault();
     if (isediting) {
       setList(
         list.map((item) => {
           if (item.id === myid) {
-            return { ...item, title: input.title, desc: input.desc };
+            return {
+              ...item,
+              title: input.title,
+              desc: input.desc,
+              color: color,
+              tag:tags.tag
+            };
           }
           return item;
         })
       );
       setisediting(false);
-      setcolor('white');
-      setInput({ id: "", title: "", desc: ""});
+      setcolor(" ");
+      setTags({tag:" "})
+      setInput({ id: "", title: "", desc: "" });
+    } else if (input.title !== "") {
      
-    } else {
-      let newdata = { ...input, id: new Date().getTime().toString(),color };
+      let newdata = { ...input, id: new Date().getTime().toString(), color,...tags };
+     
       setList([...list, newdata]);
-      setcolor('white');
-      setInput({ id: "", title: "", desc: ""});
-      
+      setcolor("white");
+      setTags({tag:""});
+      setInput({ id: "", title: "", desc: "" });
     }
   };
 
@@ -70,12 +81,10 @@ setcolor(e.target.id);
     localStorage.setItem("list", JSON.stringify(list));
   }, [list]);
 
-  
-
   return (
     <>
-      <section className="input mx-auto w-50 my-5 ">
-        <form action="" onSubmit={setdata} >
+      <section className="input mx-auto w-50 mt-5">
+        <form action="" onSubmit={setdata}>
           <div className="input-group mb-3">
             <input
               type="text"
@@ -96,28 +105,88 @@ setcolor(e.target.id);
               placeholder="write your note"
             ></textarea>
           </div>
-          
-          
         </form>
-        <div className="d-flex align-items-center mt-3">
-        <button type="submit" onClick={setdata}  className="btn btn-outline-primary my-2 me-5 d-inline">
+        <div className="d-flex align-items-center my-3">
+          <button
+            type="submit"
+            onClick={setdata}
+            className="btn btn-outline-primary my-2 me-5 d-inline"
+          >
             {!isediting ? (
               <i className="fa-solid fa-plus"></i>
             ) : (
               <i className="fa-solid fa-pen-to-square"></i>
             )}
           </button>
-          
-          <div className="d-flex mycolors " onClick={pickcolor}>
-            <button className="btn btn-outline-primar y" >select colors</button>
-            <button className="btn btn-danger mx-2 rounded-5"   id="red" ></button>
-      
-          <button className="btn btn-primary mx-2 rounded-5"  id="blue"></button>
-         
-          <button className="btn btn-dark mx-2 rounded-5"  id="dark"></button>
+
+          <div className="d-md-flex align-items-center justify-content-center mx-atuo">
+            <div
+              className="d-flex mycolors pickcolorsize  py-2 px-2"
+              onClick={pickcolor}
+            >
+              <button className="btn btn-outline-primar ">
+                select colors{" "}
+                <i className="fa-sharp fa-solid fa-paintbrush"></i>
+              </button>
+              <button
+                className="btn btn-danger mx-2 rounded-5"
+                id="red"
+              ></button>
+
+              <button
+                className="btn btn-primary mx-2 rounded-5"
+                id="blue"
+              ></button>
+
+              <button
+                className="btn btn-dark mx-2 rounded-5"
+                id="dark"
+              ></button>
+            </div>
+
+            <span className="mx-2">
+              Tag<i className="fa-sharp fa-solid fa-tag "></i>
+            </span>
+
+            <div
+              className="d-flex mycolors pickcolorsize  p-2"
+              onClick={pickTag}
+            >
+              <span
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                title="--> study time"
+              >
+                <i className="fa-solid fa-book tagicon p-2 bg-dark text-light rounded mx-2"></i>
+              </span>
+
+              <span
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                title="--> othes  work"
+              >
+                <i className="fa-solid tagicon fa-pen p-2 bg-dark text-light rounded mx-2"></i>
+              </span>
+
+              <span
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                title="--> shopping time"
+              >
+                <i className="fa-solid tagicon fa-cart-shopping p-2 bg-dark text-light rounded mx-2"></i>
+              </span>
+
+              <span
+              
+                data-bs-toggle="tooltip"
+                data-bs-placement="bottom"
+                title="--> Birthday"
+              >
+                <i className="fa-solid tagicon fa-cake-candles  p-2 bg-dark text-light rounded mx-2"></i>
+              </span>
+            </div>
           </div>
         </div>
-          
       </section>
       {/* -------- show note*/}
 
--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -4,25 +4,27 @@ import { motion } from "framer-motion";
 const Note = ({ data, deletenote, noteEdit }) => {
 
   return (
-    <div className="container ">
+    <div className="container my-5">
       <div className="row">
         {data.map((item) => {
       
-          const { id, title, desc,color } = item;
+          const { id, title, desc,color ,tag} = item;
             
           return (
             <motion.div
-              className="col-lg-4 col-md-3 col-sm-6 my-3"
+              className="col-lg-4 col-md-3  col-sm-6 my-4"
               key={id}
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-              <div className="card py-1 bg-light " id={{}}>
+              <div className="card py-1 bg-light text-light bg-dark "  id={color}>
                 <div className="card-body " >
                   <h5 className="card-title py-1">{title}</h5>
                   <p className="card-text">{desc}</p>
 
-                  <button
+                 <div className="d-flex  align-items-center justify-content-between">
+                <div >
+                <button
                     className="card-link btn btn-outline-danger "
                     onClick={() => deletenote(id)}
                   >
@@ -35,6 +37,11 @@ const Note = ({ data, deletenote, noteEdit }) => {
                   >
                     <i className="fa-solid fa-pen-to-square"></i>
                   </button>
+                </div>
+                  <span className=" mt-1">
+                 <i className={tag}></i>
+                  </span>
+                 </div>
                 </div>
               </div>
             </motion.div>
