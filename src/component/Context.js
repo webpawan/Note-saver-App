2c2bbeac93d51cc36aac68484030e86f03c4e816
webpawan <pawansaini66477@gmail.com>
2022-10-21 21:15:10 +0530

    bug fix 1.1.4

--- src/component/Context.js
+++ src/component/Context.js
@@ -1,15 +1,15 @@
-import React, { useContext,useState } from "react";
+// import React, { useContext,useState } from "react";
 
-const AppContext = React.createContext();
+// const AppContext = React.createContext();
 
-const AppProvider = ({Children}) =>{
-return <AppContext.Provider> {Children} </AppContext.Provider>
-}
+// const AppProvider = ({Children}) =>{
+// return <AppContext.Provider> {Children} </AppContext.Provider>
+// }
 
-// custome hook 
-export const useGlobalContext = () =>{
-    return useContext(AppContext)
-}
+// // custome hook 
+// export const useGlobalContext = () =>{
+//     return useContext(AppContext)
+// }
 
 
-export {AppContext,AppProvider}
+// export {AppContext,AppProvider}
